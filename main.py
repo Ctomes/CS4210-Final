f663492343e5fb2034a2afaea7d79522bcea1a7f
Ctomes <72369162+Ctomes@users.noreply.github.com>
2023-04-11 21:08:41 -0700

    Added Dataset and started a main file

---
+++ main.py
@@ -0,0 +1,7 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Christopher Tomes, Anthony Seward, Jason Rowley, ...
+# FILENAME: main.py
+# SPECIFICATION: This code will attempt to load the dataset.
+# TIME SPENT: 
+#-----------------------------------------------------------*/
+
